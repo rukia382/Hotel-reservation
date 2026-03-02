2f01b75a4d6d9106fb04feac0f2881046914597b
Rukia <hrukia382@gmail.com>
2026-03-02 11:37:33 +0300

    14 commit

--- backend/hotel/urls.py
+++ backend/hotel/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from rest_framework import permissions
+from rest_framework.response import Response
 from rest_framework.routers import DefaultRouter
+from rest_framework.reverse import reverse
+from rest_framework.views import APIView
 
 from .views import (
     BookingViewSet,
@@ -18,7 +22,27 @@ router.register(r"customers", CustomerViewSet)
 router.register(r"bookings", BookingViewSet)
 router.register(r"transactions", TransactionViewSet)
 
+
+class APIRootView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        return Response(
+            {
+                "auth-register": reverse("register", request=request),
+                "auth-login": reverse("login", request=request),
+                "auth-logout": reverse("logout", request=request),
+                "auth-me": reverse("me", request=request),
+                "rooms": reverse("room-list", request=request),
+                "customers": reverse("customer-list", request=request),
+                "bookings": reverse("booking-list", request=request),
+                "transactions": reverse("transaction-list", request=request),
+            }
+        )
+
+
 urlpatterns = [
+    path("", APIRootView.as_view(), name="api-root"),
     path("auth/register/", RegisterView.as_view(), name="register"),
     path("auth/login/", LoginView.as_view(), name="login"),
     path("auth/logout/", LogoutView.as_view(), name="logout"),
